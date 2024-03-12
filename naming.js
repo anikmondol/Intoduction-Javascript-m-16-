e874291c7523b2110b894247cc8f6df3190305ba
anikmondol <anikmondol558363@gmil.com>
2024-03-12 21:39:44 +0600

    this video about javascript keyword, variable name naming convention etc

---
+++ naming.js
@@ -0,0 +1,41 @@
+// 1. no keyword in variable name
+
+var false = 25;
+var var = 55;
+
+
+// 2. no space or gap in variable name
+
+
+var is happy = false;
+var ishappy = true;
+
+
+// 3. no quote  
+
+
+var 'address' = 'bangladesh';
+
+
+// 4. con not start with a number. but number other than the first letter is allowed
+
+var money = 45;
+var money2 = 55;
+var 3money = 88;
+
+
+// 5. variable name is case sensitive
+
+
+address;
+Address
+ADDRESS
+
+
+// 6. how to write a long variable name 
+
+var my current home address = 'mirpur' 
+var my currenthomeaddress = 'mirpur'
+var my_current_home_address = 'mirpur' // snake case
+var myCurrentHomeAddress = 'mirpur' // camel case
+var MyCurrentHomeAddress = 'mirpur' // pascal case
