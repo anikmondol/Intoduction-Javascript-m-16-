a78e2e4bb47179d5ed38dce5d8e12ae7025aa45a
anikmondol <anikmondol558363@gmil.com>
2024-03-13 00:24:11 +0600

    this video about mathematical operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,36 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+// console.log(price);
+
+
+
+
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000;
+// console.log(fridge);
+
+
+
+
+var orangePrice = 30;
+// var orangePrice = orangePrice * 3;
+orangePrice *= 3;
+// console.log(orangePrice);
+
+
+
+// ---------------------
+
+
+var first = 'Anik';
+var seconde = 'Mondal';
+
+var fullName = first + seconde;
+// console.log(fullName);
+
+
+console.log(20 + '2');
+console.log(20 - '2');
