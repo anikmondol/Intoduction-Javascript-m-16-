d87377ea6341c2abd70637fc589f7d7ae9836995
anikmondol <anikmondol558363@gmil.com>
2024-03-12 20:31:21 +0600

    this video about maths needed to srart learining programming language

---
+++ Maths.js
@@ -0,0 +1,15 @@
+/******************
+ *      MATH NEEDED FOR PROGRAMMING
+ * *****************
+ * 1. number: +ve, -ve, 0 [50, -6, 0]
+ * 2. integer: 2, 88 [odd, even]: 2, 55, 874
+ * 3. float: 5.6, 2.2
+ * 4. operation: +, -, *, /
+ * 5. more operation: % 
+ * 6. percentage: 5%. 50 take er 10% koto
+ * 7. sum: 14, 16, 18
+ * 8. avg: 14, 16, 18
+ * 9. min/max: 14, 16, 18
+ * 10. series:
+ * 11. area: Triangle
+*/
