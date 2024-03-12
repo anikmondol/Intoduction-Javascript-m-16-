a3484cf6ef29b5e7b93ee7c0fb59d68c7e371be1
anikmondol <anikmondol558363@gmil.com>
2024-03-12 20:42:59 +0600

    this video 5 things to declare a variabel

---
+++ variable.js
@@ -0,0 +1,23 @@
+/***
+ * variable ----> vary + able
+ * 5 things to declare a variable
+ * 
+ * */ 
+
+
+var weight = 50;
+var price = 35;
+var year = 2000;
+var money = 2005;
+money = 200;
+
+
+// var age;
+// console.log(age);
+// console.log(money);
+
+
+var age = 24;
+age = 20;
+
+console.log(age);
