91653cbcc442f3e83b19c006abc08c0e9f5640d6
anikmondol <anikmondol558363@gmil.com>
2024-03-12 23:40:36 +0600

    this video about javascript know the fundamentals of numbers

---
+++ number.js
@@ -0,0 +1,28 @@
+var orangePrice = 20;
+var chocolate = 0.5; 
+// var applePrice = '20'
+// var applePrice = "20";
+// var applePrice = parseInt('20');
+// var applePrice = parseInt('afasd')
+// var applePrice = parseInt('22.5')
+var applePrice = parseFloat('22.5')
+
+
+// console.log( orangePrice);
+// console.log( applePrice);
+
+// console.log(orangePrice + applePrice);
+
+
+var first = 0.1;
+var seconde = 0.2;
+var total = first + seconde
+
+console.log(total.toFixed(2));
+console.log(parseFloat(total.toFixed(1)));
+
+const num = 50 / 0;
+const num1 = -50 / 0;
+
+console.log(num);
+console.log(num1);
