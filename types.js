60f1d4d6150ede36be62d67bb3400005fda32de8
anikmondol <anikmondol558363@gmil.com>
2024-03-12 21:14:08 +0600

    this video about variable data types numeric, string, boolean etc

---
+++ types.js
@@ -0,0 +1,17 @@
+// string types Data
+var name = 'Anik';
+var address = "Dhaka";
+var country = 'Bangladesh';
+
+
+
+
+// boolean types Data 
+var isRich = false;
+var passed = true;
+
+
+
+// number types Data
+var price = 500;
+console.log(typeof price);
